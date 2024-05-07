d3b4b00f95570e5c05aa770be6419d9273c67c5f
Dalta <39882187+simonedaltardi@users.noreply.github.com>
2024-05-07 22:47:51 +0200

    Update wpDIR.py

--- wpDIR.py
+++ wpDIR.py
@@ -2,6 +2,44 @@ import argparse
 import requests
 from bs4 import BeautifulSoup
 
+# Definizione della classe per i colori del testo
+class tcolor:
+    yellow = '\33[33m'
+    red = '\33[31m'
+    green = '\33[32m'
+    end = '\33[0m'  # Per riportare il colore al valore predefinito
+
+def check_wordpress(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            meta_generator = soup.find('meta', attrs={'name': 'generator'})
+            if meta_generator and 'WordPress' in meta_generator['content']:
+                return True
+            else:
+                return False
+        else:
+            print(tcolor.red + "Errore durante il recupero del sito. Codice di stato:", response.status_code + tcolor.end)
+            return False
+    except requests.RequestException as e:
+        print(tcolor.red + "Errore durante la richiesta HTTP:", e + tcolor.end)
+        return False
+
+def check_waf(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 403:
+        print(tcolor.red + "[!] A Web Application Firewall (WAF) is possibly present, the request was blocked. [!]\n" + tcolor.end)
+    elif response.status_code != 200:
+        print(tcolor.yellow + "[!] Unable to determine if a WAF is present. Received status code:", response.status_code, "[!]\n" + tcolor.end)
+    else:
+        print(tcolor.green + "[!] No signs of WAF detected. [!]\n" + tcolor.end)
+
 def WPdir(url):
     directories = [
         "wp-admin.php",
@@ -31,59 +69,29 @@ def WPdir(url):
 
     check = 0
     for directory in directories:
-        test_url = url + directory
+        test_url = url.rstrip('/') + '/' + directory
         check_waf(test_url)
         try:
             response = requests.get(test_url)
             if response.status_code == 200:
                 check += 1
-                print("[+] Found accessible WP directory: " + test_url + " [+]\n")
+                print(tcolor.green + "[+] Found accessible WP directory:", test_url, "[+]\n" + tcolor.end)
             else:
-                print("[!] Received status code: " + str(response.status_code) + " for URL: " + test_url + " [!]\n")
+                print(tcolor.yellow + "[!] Received status code:", response.status_code, "for URL:", test_url, "[!]\n" + tcolor.end)
         except requests.exceptions.RequestException as e:
-            print("[!] Error occurred while accessing URL: " + test_url + " - " + str(e) + " [!]\n")
+            print(tcolor.red + "[!] Error occurred while accessing URL:", test_url, "-", e, "[!]\n" + tcolor.end)
 
     if check == 0:
         print("No WP directories found. :(\n")
 
-def check_waf(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-    }
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 403:
-        print("[!] Possibly a WAF is present, the request was blocked. [!]\n")
-    elif response.status_code == 200:
-        print("[!] No signs of WAF. [!]\n")
-    else:
-        print("[!] Unable to determine if a WAF is present. [!]\n")
-
-def check_wordpress(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    if soup.find('meta', attrs={'name': 'generator', 'content': 'WordPress'}) is not None:
-        return True
-
-    if soup.find('link', attrs={'rel': 'stylesheet', 'href': '/wp-content/'}) is not None:
-        return True
-
-    if soup.find('script', attrs={'src': '/wp-includes/js/wp-embed.min.js'}) is not None:
-        return True
-
-    # If none of the above conditions match, it is likely not a WordPress site
-    return False
-
-
 if __name__ == "__main__":
-   url = input("[+] Enter a valid URL [+]\n")
-   # parser = argparse.ArgumentParser(description="Descrizione")
-   # parser.add_argument('argomento1', help='Descrizione argomento1')
-   # parser.add_argument('--opzione', help='Descrizione opzionale', action='store_true')
-   # args = parser.parse_args()
-   if(check_wordpress(url)):
+    parser = argparse.ArgumentParser(description="Detect WordPress directories")
+    parser.add_argument('url', help='URL of the website to check')
+    args = parser.parse_args()
+    
+    url = args.url
+
+    if check_wordpress(url):
         WPdir(url)
-   else:
-       print("[-] This website is not a WordPress website ! [-]\n")
+    else:
+        print(tcolor.red + "[-] This website is not a WordPress website ! [-]\n" + tcolor.end)
